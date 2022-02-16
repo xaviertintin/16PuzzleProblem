78c2b9ff11e270285132fbf24eb89be737a747b7
Xavier Tintin <alextintin007@outlook.com>
2022-02-16 15:41:22 -0500

    GUI intergrated for 4x4 puzzle problem working. Bugs fixed

--- src/com/astar/GUI16Puzzle.java
+++ src/com/astar/GUI16Puzzle.java
@@ -1,10 +1,10 @@
 package com.astar;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class GUI16Puzzle extends JFrame {
-
     private JPanel panelMain;
     private JTextField txt1;
     private JTextField txt2;
@@ -24,45 +24,29 @@ public class GUI16Puzzle extends JFrame {
     private JTextField txt16;
     private JButton btnEnter;
     private JLabel lblTitle;
-    //creating new variables for input
-    public int[][] c = new int [5][4];
+    public int[][] c = new int [5][4];          //creating new array for input
 
     public GUI16Puzzle() {
         setContentPane(panelMain);
         setTitle("Proyecto Final");
         setSize(635,225);
+        CenteredFrame(this);
         setVisible(true);
-
 
         btnEnter.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                Astars a1=new Astars();
+                Astars astra=new Astars();
                 c=FillArray();
                 try {
-                    a1.getState(c);
-                    a1.display();
-                    c=a1.TreeSearch();
-                } catch (Exception ex) {
+                    astra.getState(c);
+                    astra.display();
+                    c=astra.TreeSearch();
+                    SetArray(c);
+                }
+                catch (Exception ex) {
                     System.out.println("Error");
                 }
-
-                txt1.setText(Integer.toString(c[0][0]));
-                txt2.setText(Integer.toString(c[0][1]));
-                txt3.setText(Integer.toString(c[0][2]));
-                txt4.setText(Integer.toString(c[0][3]));
-                txt5.setText(Integer.toString(c[1][0]));
-                txt6.setText(Integer.toString(c[1][1]));
-                txt7.setText(Integer.toString(c[1][2]));
-                txt8.setText(Integer.toString(c[1][3]));
-                txt9.setText(Integer.toString(c[2][0]));
-                txt10.setText(Integer.toString(c[2][1]));
-                txt11.setText(Integer.toString(c[2][2]));
-                txt12.setText(Integer.toString(c[2][3]));
-                txt13.setText(Integer.toString(c[3][0]));
-                txt14.setText(Integer.toString(c[3][1]));
-                txt15.setText(Integer.toString(c[3][2]));
-                txt16.setText(Integer.toString(c[3][3]));
             }
         });
     }
@@ -88,5 +72,29 @@ public class GUI16Puzzle extends JFrame {
         c[3][2]=Integer.parseInt(txt15.getText());
         c[3][3]=Integer.parseInt(txt16.getText());
         return c;
+    }
+    public void SetArray(int c[][]){
+        txt1.setText(Integer.toString(c[0][0]));
+        txt2.setText(Integer.toString(c[0][1]));
+        txt3.setText(Integer.toString(c[0][2]));
+        txt4.setText(Integer.toString(c[0][3]));
+        txt5.setText(Integer.toString(c[1][0]));
+        txt6.setText(Integer.toString(c[1][1]));
+        txt7.setText(Integer.toString(c[1][2]));
+        txt8.setText(Integer.toString(c[1][3]));
+        txt9.setText(Integer.toString(c[2][0]));
+        txt10.setText(Integer.toString(c[2][1]));
+        txt11.setText(Integer.toString(c[2][2]));
+        txt12.setText(Integer.toString(c[2][3]));
+        txt13.setText(Integer.toString(c[3][0]));
+        txt14.setText(Integer.toString(c[3][1]));
+        txt15.setText(Integer.toString(c[3][2]));
+        txt16.setText(Integer.toString(c[3][3]));
+    }
+    public void CenteredFrame(javax.swing.JFrame objFrame){
+        Dimension objDimension = Toolkit.getDefaultToolkit().getScreenSize();
+        int iCoordX = (objDimension.width - objFrame.getWidth()) / 2;
+        int iCoordY = (objDimension.height - objFrame.getHeight()) / 2;
+        objFrame.setLocation(iCoordX, iCoordY);
     }
 }
